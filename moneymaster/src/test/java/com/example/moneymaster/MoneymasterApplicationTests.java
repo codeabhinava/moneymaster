ea22e2d0dd978100f87b6a0781c0c0729260677e
Abhinava <cabhinava0@gmail.com>
2026-04-05 20:43:14 +0530

    User Access

---
+++ moneymaster/src/test/java/com/example/moneymaster/MoneymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
